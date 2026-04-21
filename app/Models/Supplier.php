7cb72a3eda600469b498f9ab1bb88037551a2c25
Bhavik-Gujjucoder <bhavikg.gc@gmail.com>
2026-04-21 12:16:12 +0530

    raw material and supplier model added

---
+++ app/Models/Supplier.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Supplier extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'suppliers';
+    protected $guarded = [];
+
+    public function statusBadge(): string
+    {
+        return $this->status == 1
+            ? '<span class="badge badge-pill badge-status bg-success">Active</span>'
+            : '<span class="badge badge-pill badge-status bg-danger">Inactive</span>';
+    }
+}
