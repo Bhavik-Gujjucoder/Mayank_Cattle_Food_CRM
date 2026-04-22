ebb7f38e008dbfc03f1f99ac10033f3ac21fd48b
abhayluv <abhayl.gc@gmail.com>
2026-04-22 12:41:01 +0530

    migration file change + soda/order management started

---
+++ resources/views/order_management/create.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+@section('title')
+    {{ $page_title }}
+@endsection
+@section('content')
+
+@endsection
+@section('script')
+    <script>
+        
+    </script>
+@endsection
