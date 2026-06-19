7e82594b32e2717350839ca169290fe79e7cd7c6
abhayluv <abhayl.gc@gmail.com>
2026-06-19 16:26:22 +0530

    testing for cron of due payment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -29,7 +29,7 @@ return Application::configure(basePath: dirname(__DIR__))
             ->appendOutputTo(storage_path('logs/queue.log'));
 
         $schedule->command('payment:accrue-late-fees')
-            ->dailyAt('00:00')
+            ->dailyAt('16:30')
             ->withoutOverlapping()
             ->appendOutputTo(storage_path('logs/payment-late-fees.log'));
     })
